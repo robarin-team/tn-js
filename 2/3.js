55e93e343c53a034f76bdf18925bda9a38a25b8d
Dima <sumskoydima@gmail.com>
2020-05-26 10:34:29 +0600

    Complete lesson tasks

--- 2/3.js
+++ 2/3.js
@@ -1,155 +1,3 @@
-function makeTime(hours, minutes) {
-  const date = new Date();
-  date.setHours(hours);
-  date.setMinutes(minutes);
-  date.setSeconds(0);
-  date.setMilliseconds(0);
-  return date.getTime();
-}
-
-/**
- * @type {Object<string, Flight>} Список всех рейсов
- */
-let flights = {
-  BH118: {
-    name: 'BH118',
-    seats: 28,
-    businessSeats: 4,
-    registrationStarts: makeTime(10, 0),
-    registartionEnds: makeTime(15, 0),
-    tickets: [
-      {
-        id: 'BH118-B50',
-        flight: 'BH118',
-        fullName: 'Ivanov I. I.',
-        type: 0,
-        seat: 18,
-        buyTime: makeTime(2, 0),
-        registrationTime: null,
-      }
-    ],
-  }
-};
-
-/**
- * Добавление рейса
- * 
- * * назначение номера рейса
- * * подготовка рейса
- *   * вычисление времени регистрации
- *   * подготовка структуры Flight
- * 
- * @param {Airliner} airliner Информация о самолете
- * @param {number} time Время вылета
- * @returns {Flight}
- */
-// function createFlight(airliner, time) { }
-
-/**
- * Поиск свободного места нужного типа
- * 
- * Гарантирует что найдет свободное место нужного типа или вернет null
- * 
- * @param {Flight} flight 
- * @param {number} type
- * @returns {number} seat
- */
-function findAvailableSeat(flight, type) {
-  let exists;
-  let seat;
-  let seatsOfType = 0;
-
-  switch (type) {
-    case 0: // standart
-      const availableSeats = [];
-
-      for (let i = flight.businessSeats + 1; i <= flight.seats; i++)
-        if (!flight.tickets.find(item => item.seat === i))
-          availableSeats.push(i)
-
-      if (availableSeats.length === 0)
-        return null;
-
-      const index = Math.floor(Math.random() * availableSeats.length);
-      return availableSeats[index];
-    case 1: // business
-      for (let i = 1; i <= flight.businessSeats; i++)
-        if (!flight.tickets.find(item => item.seat === i))
-          seatsOfType++;
-
-      if (seatsOfType === 0)
-        return null;
-
-      do {
-        seat = Math.floor(Math.random() * flight.businessSeats) + 1;
-        exists = flight.tickets.find(item => item.seat === seat);
-      } while (exists);
-
-      return seat;
-    default:
-      throw new Error(`Unknown type`)
-  }
-}
-
-/**
- * Покупка билета на самолет
- * 
- * * проверка рейса
- * * проверка возможности купить (время и наличие мест)
- * * сохранение данных билета в информации о рейсе
- * 
- * @param {string} flightName Номер рейса
- * @param {number} buyTime Время покупки
- * @param {string} fullName Имя пассажира
- * @param {number} type Тип места
- * @returns {Ticket} Возвращаем копию билета
- */
-function buyTicket(flightName, buyTime, fullName, type = 0) {
-  const flight = flights[flightName];
-
-  if (!flight)
-    throw new Error('Flight not found');
-
-  if (flight.tickets.length >= flight.seats)
-    throw new Error('No seats available');
-
-  if (buyTime > flight.registartionEnds)
-    throw new Error('Time away');
-
-  const seat = findAvailableSeat(flight, type);
-  if (!seat)
-    throw new Error(`No seats of type ${type} available. You can choose another type`);
-
-  let id;
-  do {
-    id = flight.name + '-' + Math.random().toString().substr(2, 3);
-    exists = flight.tickets.find(item => item.id === id);
-  } while (exists);
-
-  /**
-   * @type {Ticket}
-   */
-  const ticket = {
-    id,
-    flight: flight.name,
-    buyTime,
-    fullName,
-    registrationTime: null,
-    type,
-    seat,
-  }
-
-  flight.tickets.push(ticket);
-
-  // return Object.assign({}, ticket);
-  return {
-    ...ticket,
-  };
-}
-
-const ticket = buyTicket('BH118', makeTime(5, 10), 'Petrov I. I.');
-console.log(ticket);
-
 /**
  * Функция пробует произвести электронную регистрацию пассажира
  * 
@@ -172,6 +20,12 @@ function eRegistration(ticketId, fullName, nowTime) {
     if (!flight)
       throw new Error('Flight not found');
 
+    if (nowTime < flight.registrationStarts)
+      throw new Error('Check-in has not started yet');
+
+    if (nowTime >= flight.registartionEnds)
+      throw new Error('Check-in is over');
+
     const ticket = flight.tickets.find(ticket => ticket.id === ticketId);
 
     if (!ticket)
@@ -180,19 +34,15 @@ function eRegistration(ticketId, fullName, nowTime) {
     if (ticket.fullName !== fullName)
       throw new Error('Invalid name');
 
-    if (nowTime < flight.registrationStarts)
-      throw new Error('Check-in has not started yet');
-
-    if (nowTime >= flight.registartionEnds)
-      throw new Error('Check-in is over');
-
     ticket.registrationTime = nowTime;
 
     return true;
-  } catch(e) {
+  } catch (e) {
     console.log(e.message)
   }
 }
+
+ticket = buyTicket('BH118', makeTime(5, 10), 'Petrov I. I.');
 
 console.log(eRegistration(ticket.id, ticket.fullName, new Date().getTime()));
 console.log(eRegistration('BH118-123', ticket.fullName, new Date().getTime()));
