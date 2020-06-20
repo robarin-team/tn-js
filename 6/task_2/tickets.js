0e45a6b2f385bdf91c36cbfd7e2f91664b4ac30a
Dmitry Sumskoy <sumskoydima@gmail.com>
2020-06-20 18:47:59 +0600

    Add task_2

---
+++ 6/task_2/tickets.js
@@ -0,0 +1,35 @@
+const buyTicketform = document.getElementById("buy_ticket");
+
+buyTicketform.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const form = e.target;
+  const flightFields = ['flightName', 'type'];
+  const userFields = ['firstName', 'lastName', 'middleName'];
+
+  const userName = userFields.map(field => {
+    if (form.elements[field]) {
+      return form.elements[field].value;
+    }
+  }).join(' ');
+
+  const ticketData = flightFields.reduce((data, fieldName) => {
+    data[fieldName] = form.elements[fieldName].value;
+    return data;
+  }, {});
+
+  ticketData.fullName = userName;
+  ticketData.buyTime = makeTime(11, 0);
+
+  try {
+    const { flightName, buyTime, fullName, type } = ticketData;
+    
+    const result = buyTicket(flightName, buyTime, fullName, parseInt(type));
+    const { seat, id } = result;
+
+    alert(`Your seat: ${seat}, ticket number: ${id}`);
+    form.reset();
+  } catch(e) {
+    alert(e.message);
+  }
+});
